38a2940f6391e1f28e184734cb5292ca51bb457b
Helena <helena.soukupova5@gmail.com>
2020-10-18 18:20:14 -0700

    Redesign main page

--- src/ui_components/login_form/login_form.js
+++ src/ui_components/login_form/login_form.js
@@ -5,8 +5,8 @@ import SubmitButton from '../submit_button/submit_button';
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleLogin}>
-      <input placeholder="Email" type="email" name="emailAddress"/><br/>
-      <input placeholder="Password" type="password" name="password"/><br/><br/>
+      <input id="Email" placeholder="Email" type="email" name="emailAddress"/><br/>
+      <input id="Password" placeholder="Password" type="password" name="password"/><br/><br/>
       <SubmitButton text="Log In"/>
     </form>
   );
