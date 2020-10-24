86858fb9ef734966eeeb67a4110bd915fc3d47e7
Helena <helena.soukupova5@gmail.com>
2020-10-23 19:51:47 -0700

    cancel_button used in otp_modal

---
+++ src/ui_components/cancel_button/cancel_button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CancelButton = (props) => {
+  return <button className="cancel-button">{props.text}</button>
+}
+
+export default CancelButton
--- src/ui_components/modals/otp_modal.js
+++ src/ui_components/modals/otp_modal.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import CancelButton from '../cancel_button/cancel_button';
 import SubmitButton from '../submit_button/submit_button';
 
 const OTPModal = (props) => {
   return props.isOpen ? (
     <div className="modal-background">
       <div className="modal-card">
+        <CancelButton  text="X" />
         <form className="otp-input-form" onSubmit={props.action}>
-          <input className="otp-input-form-field" placeholder="One Time Password" name="otp"/><br/>
-          <SubmitButton text="Confirm"/>
+          <input className="otp-input-form-field onelogin-input" placeholder="One Time Password" name="otp"/><br/>
+          <SubmitButton text="Confirm"/>&nbsp;
+          <SubmitButton text="Cancel" />
         </form>
       </div>
     </div>
