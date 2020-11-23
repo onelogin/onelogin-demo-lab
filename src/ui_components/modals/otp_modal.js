8236cb03a9493ec9096f1ea15201e8aeca51e712
Dominick Caponi <d.caponi1@gmail.com>
2020-11-23 12:22:02 -0800

    reduces tab spacing in otp_modal

--- src/ui_components/modals/otp_modal.js
+++ src/ui_components/modals/otp_modal.js
@@ -3,16 +3,16 @@ import React, { Component} from 'react';
 import SubmitButton from '../buttons/submit_button';
 
 const OTPModal = ({close, action}) => (
-      <div className="modal-background">
-        <div className="modal-card">
-        <button className="cancel-button" onClick={close}>X</button>
-          <form className="otp-input-form" onSubmit={action}>
-            <input className="otp-input-form-field onelogin-input" placeholder="One Time Password" name="otp"/><br/>
-            <SubmitButton text="Confirm"/>&nbsp;
-            <button className="submit-button" onClick={close}>Cancel</button>
-          </form>
-        </div>
-      </div>
-  )
+  <div className="modal-background">
+    <div className="modal-card">
+    <button className="cancel-button" onClick={close}>X</button>
+      <form className="otp-input-form" onSubmit={action}>
+        <input className="otp-input-form-field onelogin-input" placeholder="One Time Password" name="otp"/><br/>
+        <SubmitButton text="Confirm"/>&nbsp;
+        <button className="submit-button" onClick={close}>Cancel</button>
+      </form>
+    </div>
+  </div>
+)
 
 export default OTPModal;
